a89bf795a9138790ce15450f6cfd23481ce06555
Khalid Bouychou <khbouych@e3r10p18.1337.ma>
2024-09-06 17:50:39 +0100

    update

---
+++ back/api/serializers.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+        def create(self,validatedata):
+            user = User.objects.create_user(**validatedata)
+            return user
---
+++ back/api/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.contrib.auth.models import User
+from rest_framework import generics 
+from .serializers import UserSerializer
+
+from  rest_framework.permissions import IsAuthenticated , AllowAny
+
+
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all() # get all the users
+    serializer_class = User # use the UserSerializer
+    permission_classes = (AllowAny,) # allow any user to access this view
