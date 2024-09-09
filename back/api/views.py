54fdba37f13dee27a1224b73ae0bbb3a1ec94097
khalid bouychou <khalidbouychou22@gmail.com>
2024-09-09 18:55:01 +0100

    chore: Update dependencies and imports in views.py and requirements.txt

--- back/api/views.py
+++ back/api/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 
 from django.contrib.auth.models import User
-from rest_framework import generics 
+from rest_framework import generics
 from .serializers import UserSerializer
 
 from  rest_framework.permissions import IsAuthenticated , AllowAny
